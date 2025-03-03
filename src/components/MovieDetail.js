9ef17750cb12771eb374f9565df45214ac366e72
suluuelif <esu3@uncc.edu>
2025-03-02 21:55:28 -0500

    n

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,14 +1,46 @@
 // src/components/MovieDetail.js
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import moviesData from '../data/moviesData';
+import { useState, useEffect } from 'react';
+//import moviesData from '../data/moviesData';
 import '../App.css';
 
 function MovieDetail() {
   const { id } = useParams();
-  const movie = moviesData.find(movie => movie.id === parseInt(id));
+  const [movie, setMovie] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+  
+useEffect(()=>
+  fetch(`http://www.omdbapi.com/?i=${id}&apikey=3273b7f6`)
+    .then(response => response.json())
+    .then(data => {
+      if(data.Response){
+        console.log(data);
+      setMovie(data);
+      setError('');
+      }
+      else{
+        setError('No movie Found')
+      }
+      setLoading(false);
+    
+    })
+      .catch(() => {
+        setError('Error fetching movies.')
+       }), [id])
 
-  if (!movie) return <h2>Movie not found</h2>;
+       if(loading) return <p>Movies are loading...</p>
+       
+       if(error)return <p>{error}</p>
+
+
+
+    //const movie = moviesData.find(movie => movie.id === imdbID);
+
+  
+
+  //if (!movie) return <h2>Movie not found</h2>;
 
   return (
     <div>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,23 +1,59 @@
 // src/components/MovieList.js
-import React, { useState } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import moviesData from '../data/moviesData';
+//import moviesData from '../data/moviesData';
 
 function MovieList() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [moviesData, setMoviesData] = useState([]);
   const [filteredMovies, setFilteredMovies] = useState(moviesData);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
+  useEffect(()=>
+    fetch(`http://www.omdbapi.com/?s=potter&apikey=3273b7f6`)
+      .then(response => response.json())
+      .then(data => {
+        if(data.Search){
+          console.log(data);
+        setMoviesData(data);
+        setError('');
+        }
+        else{
+          setError('No movies Found')
+        }
+        setLoading(false);
+      
+      })
+
+      .catch(() => {
+        setError('Error fetching movies.')
+       }), [])
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
     filterMovies(event.target.value);
   };
+
+  useEffect(() =>{
+    setFilteredMovies(moviesData);
+  }, [moviesData]);
 
   const filterMovies = (term) => {
     const filtered = moviesData.filter(movie =>
       movie.title.toLowerCase().includes(term.toLowerCase())
     );
     setFilteredMovies(filtered);
+    setLoading(false);
   };
+
+  if(loading){
+    return <p>Movies are loading...</p>
+  }
+
+  if(error){
+    return <p>Error...</p>
+  }
 
   return (
     <div className="movie-list">
@@ -30,9 +66,9 @@ function MovieList() {
       />
       <ul>
         {filteredMovies.map(movie => (
-          <li key={movie.id}>
-            <Link to={`/movies/${movie.id}`}>
-              {movie.title} ({movie.year})
+          <li key={movie.imdbID}>
+            <Link to={`/movies/${movie.imdbID}`}>
+              {movie.Title} ({movie.Year})
             </Link>
           </li>
         ))}
